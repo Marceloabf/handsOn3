3fe9c7e412723a172b86e12fe5b783aec52d924f
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-05-30 16:46:11 -0300

    Melhoria da autenticação da data, levando em consideração agora uma data minima de agora

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,11 @@
-const Atendimento = require('../models/Atendimento');
-const Psicologo = require('../models/Psicologo');
-const Paciente = require('../models/Paciente');
+const Atendimento = require('../models/Atendimento')
+const Psicologo = require('../models/Psicologo')
+const Paciente = require('../models/Paciente')
 
+Atendimento.belongsTo(Paciente, { foreignKey: 'paciente_id' })
+Paciente.hasMany(Atendimento, { foreignKey: 'paciente_id' })
 
-Atendimento.belongsTo(Paciente,{foreignKey:"paciente_id"});
-Paciente.hasMany(Atendimento, {foreignKey:"paciente_id"});
+Atendimento.belongsTo(Psicologo, { foreignKey: 'psicologo_id' })
+Psicologo.hasMany(Atendimento, { foreignKey: 'psicologo_id' })
 
-
-Atendimento.belongsTo(Psicologo, {foreignKey:"psicologo_id"});
-Psicologo.hasMany(Atendimento, {foreignKey:"psicologo_id"});
-
-module.exports = {Atendimento,Paciente,Psicologo}
+module.exports = { Atendimento, Paciente, Psicologo }
--- src/validations/atendimento/create.js
+++ src/validations/atendimento/create.js
@@ -1,9 +1,9 @@
-const {validate, Joi} = require("express-validation")
+const { validate, Joi } = require('express-validation')
 
 module.exports = validate({
-    body: Joi.object({
-        paciente_id: Joi.number().integer().required(),
-        data_atendimento: Joi.string().required(),
-        observacao:Joi.string().required()
-    })
-})  
+  body: Joi.object({
+    paciente_id: Joi.number().integer().required(),
+    data_atendimento: Joi.date().min('now').required(),
+    observacao: Joi.string().required()
+  })
+})
